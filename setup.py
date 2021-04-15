221c17e9293a71092dca05917f164df9798ee638
sec573 <573@573.local>
2021-04-15 07:48:54 -0400

    bump minor revision number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 copyfile('pyWars.py', os.path.join(get_python_lib(), 'pyWars.py'))
 setup(
     name="pywars", 
-    version="4.0.2", 
+    version="4.0.3", 
     install_requires = ["requests >= 2.20.0", "rich >= 9.1.0"],
     license="All Rights Reserved. Do not distribute.",
     url="https://github.com/markbaggett/pyWars"

345824c6d836123c1135ccae1df47dab33c28016
sec573 <573@573.local>
2022-12-01 08:57:52 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="pywars", 
-    version="5.0.0", 
+    version="5.0.1", 
     install_requires = ["requests >= 2.20.0", "rich >= 9.1.0"],
     license="All Rights Reserved. Do not distribute.",
     url="https://github.com/markbaggett/pyWars",
